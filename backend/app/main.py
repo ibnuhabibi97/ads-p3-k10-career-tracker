8bf6fee14c4c137b82223d14ddaf9f74749b20f6
Ibnu Burhanudin Habibi <burhanudinibnu105@gmail.com>
2026-04-27 22:03:54 +0700

    feat: add lowongan router to main.py

---
+++ backend/app/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from app.api.v1.routers import lowongan_router
+from app.db.database import engine, Base
+
+
+app = FastAPI(
+    title="Sistem Informasi Magang IPB API",
+    description="Backend service untuk sistem pendaftaran dan penilaian magang mahasiswa.",
+    version="1.0.0"
+)
+
+# Mendaftarkan router
+app.include_router(lowongan_router.router, prefix="/api/v1")
+
+@app.get("/")
+def root():
+    return {"message": "Selamat datang di API Sistem Informasi Magang"}
