947cd4904e0aef4fc6dbd6ee369628ddc55c65e0
Ibnu Burhanudin Habibi <burhanudinibnu105@gmail.com>
2026-04-27 21:07:29 +0700

    feat: add laporan model

---
+++ backend/app/models/laporan.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, Integer, String, Date, ForeignKey
+from sqlalchemy.orm import relationship
+from app.models.base import Base
+import datetime
+
+class Laporan(Base):
+    __tablename__ = "laporan"
+
+    id_laporan = Column(Integer, primary_key=True, index=True)
+    mahasiswa_id = Column(Integer, ForeignKey("mahasiswa.user_id"), nullable=False)
+    dosen_id = Column(Integer, ForeignKey("dosen.user_id"), nullable=True)
+    
+    status = Column(String, default="Menunggu Nilai") # Menunggu Nilai / Telah Dinilai
+    nilai = Column(Integer, nullable=True)
+    tanggal_lapor = Column(Date, default=datetime.date.today)
+    catatan = Column(String, nullable=True)
+    dokumen_laporan = Column(String, nullable=False) # Path/URL file
+
+    # Relasi
+    mahasiswa = relationship("Mahasiswa", back_populates="laporans")
+    dosen = relationship("Dosen", back_populates="laporans_dinilai")
