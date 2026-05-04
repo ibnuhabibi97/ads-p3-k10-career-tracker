23d6a30e991f44acc04bfea8b79a0f8c0d2e930a
Ibnu Burhanudin Habibi <burhanudinibnu105@gmail.com>
2026-05-05 06:46:37 +0700

    feat: update lowongan router

--- backend/app/api/v1/routers/lowongan_router.py
+++ backend/app/api/v1/routers/lowongan_router.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 from typing import List
 
 from app.db.database import get_db
-from app.schemas.lowongan_schema import LowonganCreate, LowonganResponse
+from app.schemas.lowongan_schema import LowonganCreate, LowonganUpdate, LowonganResponse
 from app.services.lowongan_service import LowonganService
 
 router = APIRouter(
@@ -16,22 +16,27 @@ def get_all_lowongan(db: Session = Depends(get_db)):
     service = LowonganService(db)
     return service.ambil_semua_lowongan()
 
-@router.get("/{id_lowongan}", response_model=LowonganResponse)
-def get_lowongan_by_id(id_lowongan: int, db: Session = Depends(get_db)):
+@router.get("/aktif", response_model=List[LowonganResponse])
+def get_lowongan_aktif(db: Session = Depends(get_db)):
     service = LowonganService(db)
-    return service.ambil_lowongan_by_id(id_lowongan)
+    return service.ambil_lowongan_aktif()
+
+@router.get("/{lowongan_id}", response_model=LowonganResponse)
+def get_lowongan_by_id(lowongan_id: int, db: Session = Depends(get_db)):
+    service = LowonganService(db)
+    return service.ambil_lowongan_by_id(lowongan_id)
 
 @router.post("/", response_model=LowonganResponse, status_code=201)
 def create_lowongan(lowongan: LowonganCreate, db: Session = Depends(get_db)):
     service = LowonganService(db)
     return service.tambah_lowongan(lowongan)
 
-@router.put("/{id_lowongan}", response_model=LowonganResponse)
-def update_lowongan(id_lowongan: int, lowongan: LowonganCreate, db: Session = Depends(get_db)):
+@router.put("/{lowongan_id}", response_model=LowonganResponse)
+def update_lowongan(lowongan_id: int, lowongan: LowonganUpdate, db: Session = Depends(get_db)):
     service = LowonganService(db)
-    return service.ubah_lowongan(id_lowongan, lowongan)
+    return service.ubah_lowongan(lowongan_id, lowongan)
 
-@router.delete("/{id_lowongan}")
-def delete_lowongan(id_lowongan: int, db: Session = Depends(get_db)):
+@router.delete("/{lowongan_id}")
+def delete_lowongan(lowongan_id: int, db: Session = Depends(get_db)):
     service = LowonganService(db)
-    return service.hapus_lowongan(id_lowongan)
+    return service.hapus_lowongan(lowongan_id)
