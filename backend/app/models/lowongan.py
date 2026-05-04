01a5aa69527af0c65c33df4f479c4e5325c337eb
Ibnu Burhanudin Habibi <burhanudinibnu105@gmail.com>
2026-05-04 16:36:03 +0700

    feat: update lowongan model

--- backend/app/models/lowongan.py
+++ backend/app/models/lowongan.py
@@ -1,16 +1,18 @@
 from sqlalchemy import Column, Integer, String, Boolean, Date
 from sqlalchemy.orm import relationship
 from app.models.base import Base
+from sqlalchemy.dialects.postgresql import ARRAY
 
 class Lowongan(Base):
     __tablename__ = "lowongan"
 
-    id_lowongan = Column(Integer, primary_key=True, index=True)
-    judul = Column(String, index=True, nullable=False)
-    deskripsi = Column(String, nullable=False)
-    persyaratan = Column(String, nullable=False)
+    lowongan_id = Column(Integer, primary_key=True, index=True)
+    perusahaan = Column(String, nullable=False)
+    judul_posisi = Column(String, index=True, nullable=False)
+    deskripsi_pekerjaan = Column(String, nullable=False)
+    kualifikasi = Column(ARRAY(String), nullable=False)
     deadline = Column(Date, nullable=False)
-    status = Column(Boolean, default=True)
+    is_active = Column(Boolean, default=True)
 
     # Relasi
     pendaftaran = relationship("Pendaftaran", back_populates="lowongan")
