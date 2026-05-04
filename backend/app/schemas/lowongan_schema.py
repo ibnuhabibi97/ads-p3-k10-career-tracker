9eb4d6440dca33cc0c480d0fc768cedac77fe3de
Ibnu Burhanudin Habibi <burhanudinibnu105@gmail.com>
2026-05-04 21:56:23 +0700

    feat: update lowongan schema

--- backend/app/schemas/lowongan_schema.py
+++ backend/app/schemas/lowongan_schema.py
@@ -1,17 +1,34 @@
 from pydantic import BaseModel, ConfigDict
+from typing import Optional
 from datetime import date
 
+#Base Schema
 class LowonganBase(BaseModel):
-    judul: str
-    deskripsi: str
-    persyaratan: str
+    perusahaan: str
+    judul_posisi: str
+    deskripsi_pekerjaan: str
+    kualifikasi: list[str]  # ARRAY(String) di SQLAlchemy = list[str] di Pydantic
     deadline: date
-    status: bool = True
+    is_active: bool = True
 
+
+#Create Schema (Untuk POST Request)
 class LowonganCreate(LowonganBase):
     pass
 
+
+#Update Schema (Untuk PUT/PATCH Request)
+class LowonganUpdate(BaseModel):
+    perusahaan: Optional[str] = None
+    judul_posisi: Optional[str] = None
+    deskripsi_pekerjaan: Optional[str] = None
+    kualifikasi: Optional[list[str]] = None
+    deadline: Optional[date] = None
+    is_active: Optional[bool] = None
+
+
+#Response / In-DB Schema (Untuk Return API)
 class LowonganResponse(LowonganBase):
-    id_lowongan: int
-    
+    lowongan_id: int
+
     model_config = ConfigDict(from_attributes=True)
