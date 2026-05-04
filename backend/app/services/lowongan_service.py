196400b069979c1f0da4face850ef5a307f49108
Ibnu Burhanudin Habibi <burhanudinibnu105@gmail.com>
2026-05-05 06:43:11 +0700

    feat: update lowongan service

--- backend/app/services/lowongan_service.py
+++ backend/app/services/lowongan_service.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import Session
 from fastapi import HTTPException
 from app.repositories.lowongan_repository import LowonganRepository
-from app.schemas.lowongan_schema import LowonganCreate
+from app.schemas.lowongan_schema import LowonganCreate, LowonganUpdate
 
 class LowonganService:
     def __init__(self, db: Session):
@@ -10,24 +10,26 @@ class LowonganService:
     def ambil_semua_lowongan(self):
         return self.repo.get_all()
 
-    def ambil_lowongan_by_id(self, id_lowongan: int):
-        lowongan = self.repo.get_by_id(id_lowongan)
+    def ambil_lowongan_aktif(self):
+        return self.repo.get_active()
+
+    def ambil_lowongan_by_id(self, lowongan_id: int):
+        lowongan = self.repo.get_by_id(lowongan_id)
         if not lowongan:
             raise HTTPException(status_code=404, detail="Data lowongan tidak ditemukan")
         return lowongan
 
     def tambah_lowongan(self, data: LowonganCreate):
-        # Di sini kamu bisa menambahkan validasi bisnis khusus jika diperlukan di masa depan
         return self.repo.create(data)
 
-    def ubah_lowongan(self, id_lowongan: int, data: LowonganCreate):
-        lowongan = self.repo.update(id_lowongan, data)
+    def ubah_lowongan(self, lowongan_id: int, data: LowonganUpdate):
+        lowongan = self.repo.update(lowongan_id, data)
         if not lowongan:
             raise HTTPException(status_code=404, detail="Data lowongan gagal diubah karena tidak ditemukan")
         return lowongan
 
-    def hapus_lowongan(self, id_lowongan: int):
-        berhasil = self.repo.delete(id_lowongan)
+    def hapus_lowongan(self, lowongan_id: int):
+        berhasil = self.repo.delete(lowongan_id)
         if not berhasil:
             raise HTTPException(status_code=404, detail="Data lowongan gagal dihapus karena tidak ditemukan")
         return {"message": "Data lowongan berhasil dihapus"}
