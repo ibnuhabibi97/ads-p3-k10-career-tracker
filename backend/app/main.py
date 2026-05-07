de60ae21f22e2af7b651303b16e778b777d82c4f
Ibnu Burhanudin Habibi <burhanudinibnu105@gmail.com>
2026-05-08 02:03:47 +0700

    feat: include auth router in main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.api.v1.routers import lowongan_router
 from app.db.database import engine
 from app.models import Base
+from app.api.v1.routers import auth_router
 
 
 app = FastAPI(
@@ -12,6 +13,7 @@ app = FastAPI(
 
 # Mendaftarkan router
 app.include_router(lowongan_router.router, prefix="/api/v1")
+app.include_router(auth_router.router, prefix="/api/v1")
 
 @app.get("/")
 def root():
