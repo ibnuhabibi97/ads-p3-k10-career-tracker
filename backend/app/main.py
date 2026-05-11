415740eff03e78d38d7c41994c523b57d5ba6a18
Ibnu Burhanudin Habibi <burhanudinibnu105@gmail.com>
2026-05-11 11:16:17 +0700

    feat: include pendaftarn router

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,6 +3,7 @@ from app.api.v1.routers import lowongan_router
 from app.db.database import engine
 from app.models import Base
 from app.api.v1.routers import auth_router
+from app.api.v1.routers import pendaftaran_router
 
 
 app = FastAPI(
@@ -14,6 +15,7 @@ app = FastAPI(
 # Mendaftarkan router
 app.include_router(lowongan_router.router, prefix="/api/v1")
 app.include_router(auth_router.router, prefix="/api/v1")
+app.include_router(pendaftaran_router.router, prefix="/api/v1")
 
 @app.get("/")
 def root():
